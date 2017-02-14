289432db7bc050af0980a2472cfd2c0d3a7417b2
Andy Doyle <andy@localhost.localdomain>
2017-02-14 01:02:59 -0500

    Started constructing template into pug modules, and for grunt to render the pug files into html

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+var moment = require('moment');
+
+var site = (function(site) {
+  site.weddingDate = moment(site.weddingDate);
+
+  return site;
+})(require('./src/site.json'));
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    pug: {
+      deploy: {
+        options: {
+          data: site
+        },
+        files: {
+          'deploy/index.html': ['src/views/site.pug']
+        }
+      }
+    },
+    wiredep: {
+      deploy: {
+        src: [ 'deploy/index.html' ],
+        overrides: {
+          jquery: { main: ['dist/jquery.min.js'] },
+          bootstrap: { main: ['dist/css/bootstrap.min.css', 'dist/js/bootstrap.min.js'] },
+          timecircles: { main: ['inc/TimeCircles.css', 'inc/TimeCircles.js']},
+          'jquery.selectbox': { main: ['css/jquery.selectbox.css', 'js/jquery.selectbox-0.2.min.js']},
+          'font-awesome': { main: ['css/font-awesome.min.css']},
+          wow: { main: ['dist/wow.min.js'] },
+          masonry: { main: ['dist/masonry.pkgd.min.js'], dependencies: []},
+          imagesloaded: { main: ['imagesloaded.pkgd.min.js'], dependencies: []}
+        }
+      }
+    },
+    copy: {
+      deploy: {
+        files: [
+          {
+            expand: true,
+            src: 'assets/**',
+            dest: 'deploy/'
+          }
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-pug');
+  grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default', ['pug', 'wiredep', 'copy']);
+};
